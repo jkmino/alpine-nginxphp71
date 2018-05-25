cbe6cfa8253f6c1c22aacb817b780528dfa754e6
jcamino <josekmino@gmail.com>
2018-05-25 18:02:18 -0500

    chmod -R 0600 ~/.ssh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Disable Strict Host checking for non interactive git clones
 mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
-
+chmod -R 0600 /root/.ssh
 # Tweak nginx to match the workers to cpu's
 procs=$(cat /proc/cpuinfo |grep processor | wc -l)
 sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
